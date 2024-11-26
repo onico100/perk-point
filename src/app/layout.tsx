9035c2b2f3d006086dadde2594b2bd4ff41bd1ba
Tamar-Amar <amtamar747@gmail.com>
2024-11-26 15:10:13 +0200

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,33 @@
 "use client"
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import SideBar from "@/components/Bars/SideBar";
 
+//אל תמחקו, שישאר לדוגמא לשימוש שלנו
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+
+const heebo= localFont({
+  src: "./fonts/HeeboVF.ttf",
+  variable: "--font-heebo",
   weight: "100 900",
 });
-
 
 
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   const queryClient = new QueryClient();
   return (
-    <html lang="en">  
+    <html lang="en" className={`${geistSans.variable} ${heebo.variable}`}>  
     <QueryClientProvider client={queryClient}>
-      <body>
+      <body className="font-heebo">
+        <p>שלומות</p>
         {children}
+        <SideBar />
       </body>
     </QueryClientProvider>
     </html>
