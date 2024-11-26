cae6e1c84e4ec6e4c9dfada8dc0294981f38060d
Tamar-Amar <amtamar747@gmail.com>
2024-11-26 15:31:10 +0200

    topBarCss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${heebo.variable}`}>  
     <QueryClientProvider client={queryClient}>
       <body className="font-heebo">
-        <p>שלומות</p>
+        <p>Hello from layout</p>
+        <TopBar />
         {children}
       </body>
     </QueryClientProvider>
