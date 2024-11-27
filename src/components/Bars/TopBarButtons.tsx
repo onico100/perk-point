820305d2a0147f621e1c12f17f1715bea934e593
onico100 <142163577+onico100@users.noreply.github.com>
2024-11-27 14:21:50 +0200

    dropdown

--- src/components/Bars/TopBarButtons.tsx
+++ src/components/Bars/TopBarButtons.tsx
@@ -1,14 +1,11 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Bars/TopBar.module.css";
 import ModePopup from "./ModePopup";
 import useGeneralStore from "@/stores/generalStore";
 import useUserStore from "@/stores/usersStore";
 import useSupplierStore from "@/stores/supplierStore";
 
-
-
 const TopBarButtons: React.FC = () => {
-
   const { clientMode } = useGeneralStore();
   const { supplier } = useSupplierStore();
   const { user } = useUserStore();
@@ -28,28 +25,37 @@ const TopBarButtons: React.FC = () => {
 
   return (
     <>
-    {clientMode === "GENERAL" && (
-      <div className={styles.buttonsContainer}>
-        <button className={styles.loginButton} onClick={handleButtonClick}>התחברות</button>
-        <button className={styles.registerButton} onClick={handleButtonClick}>הרשמה</button>
-        {popupVisible && <ModePopup onClose={handleClosePopup} anchorElement={anchorElement} />}
-      </div>
-    )}
+      {clientMode === "GENERAL" && (
+        <div className={styles.buttonsContainer}>
+          <button className={styles.loginButton} onClick={handleButtonClick}>
+            התחברות
+          </button>
+          <button className={styles.registerButton} onClick={handleButtonClick}>
+            הרשמה
+          </button>
+          {popupVisible && (
+            <ModePopup
+              onClose={handleClosePopup}
+              anchorElement={anchorElement}
+            />
+          )}
+        </div>
+      )}
 
-    {clientMode === "USER" && (
-      <div className={styles.buttonsContainer}>
-        <h1>{`שלום ${user?.username}`}</h1>
-        <button className={styles.registerButton}>התנתקות</button>
-      </div>
-    )}
+      {clientMode === "USER" && (
+        <div className={styles.buttonsContainer}>
+          <h1>{`שלום ${user?.username}`}</h1>
+          <button className={styles.registerButton}>התנתקות</button>
+        </div>
+      )}
 
-    {clientMode === "SUPPLIER" && (
-      <div className={styles.buttonsContainer}>
-        <h1>{`שלום ${supplier?.providerName}`}</h1>
-        <button className={styles.registerButton}>התנתקות</button>
-      </div>
-    )}
-  </>
+      {clientMode === "SUPPLIER" && (
+        <div className={styles.buttonsContainer}>
+          <h1>{`שלום ${supplier?.providerName}`}</h1>
+          <button className={styles.registerButton}>התנתקות</button>
+        </div>
+      )}
+    </>
   );
 };
 
