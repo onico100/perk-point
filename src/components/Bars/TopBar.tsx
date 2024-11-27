ae37bf245cf3f03e9fb16ac312f392036d8e1332
batya <batyah333@gmail.com>
2024-11-27 11:48:53 +0200

    style

--- src/components/Bars/TopBar.tsx
+++ src/components/Bars/TopBar.tsx
@@ -1,18 +1,22 @@
 "use client";
 import React from "react";
-import "@/styles/Bars/TopBar.css"; // Import styles
 import TopBarButtons from "./TopBarButtons"; // Import the Buttons component
 import Link from "next/link";
-import logoLight from "@/assets/logoLight.png";
+import logoLight from "@/assets/logoLight.png"
+import linkStyle from "@/styles/Bars/Links.module.css";
+import { usePathname } from "next/navigation";
+import styles from '@/styles/Bars/TopBar.module.css';
+
 
 const TopBar: React.FC = () => {
+  const pathname = usePathname();
     return (
-        <div className="top-bar">
-            <div className="right-side">
-                <img src={logoLight.src} alt="Perk Point Logo" className="logo" />
-                <Link href={"/benefits/0"}>הטבות</Link>
-                <Link href={"/clubs/0"}>מועודנים</Link>
-                <Link href={"/about"}>אודות</Link>
+        <div className={styles.topBar}>
+            <div className={styles.rightSide}>
+                <img src={logoLight.src} alt="Perk Point Logo" className={styles.logo} />
+                <Link className= {`${linkStyle.barItem}  ${pathname ===  "/benefits/0"? linkStyle.active : ''}`} href={"/benefits/0"}>הטבות</Link>
+                <Link className= {`${linkStyle.barItem}  ${pathname ===  "/clubs/0"? linkStyle.active : ''}`} href={"/clubs/0"}>מועודנים</Link>
+                <Link className={`${linkStyle.barItem}  ${pathname ===  "/about"? linkStyle.active : ''}`} href={"/about"}>אודות</Link>
             </div>
             <TopBarButtons />
         </div>
@@ -20,3 +24,4 @@ const TopBar: React.FC = () => {
 };
 
 export default TopBar;
+7 
