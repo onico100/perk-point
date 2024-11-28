df6e64c53807efa9e05d19c2cc7f81abe94c05a9
Batya Cohen <batyah333@gmail.com>
2024-11-28 14:35:35 +0200

    build benefit show

--- src/components/Benefits/BenefitDetails.tsx
+++ src/components/Benefits/BenefitDetails.tsx
@@ -1,39 +1,51 @@
 "use client";
 import { useFetchBenefits } from "@/hooks/useFetchBenefits";
-import { useEffect } from "react";
-import { useFetchSupplier } from "@/hooks/useFetchSupplier";
-import {useFetchGeneral} from "@/hooks/useFetchGeneral"
-import { Benefit, Supplier, Club } from "@/types/types"; 
-
+import { useFetchSuppliers } from "@/hooks/useFetchSuppliers";
+import { useFetchGeneral } from "@/hooks/useFetchGeneral";
+import { Benefit, Supplier, Club } from "@/types/types";
 
 const BenefitDetails = () => {
-    const { benefits, isLoadingB, isFetchingB } = useFetchBenefits();
-    const { suppliers, isLoadingS, isFetchingS } = useFetchSupplier();
-    const { clubs, isLoadingC, isFetchingC } = useFetchGeneral();
+  const { benefits, isLoadingB, isFetchingB } = useFetchBenefits();
+  const { suppliers, isLoadingS, isFetchingS } = useFetchSuppliers();
+  const { clubs, isLoadingC, isFetchingC } = useFetchGeneral();
 
+  if (
+    isLoadingB ||
+    isFetchingB ||
+    isLoadingS ||
+    isFetchingS ||
+    isLoadingC ||
+    isFetchingC
+  )
+    return <div>Loading...</div>;
 
-    if (isLoadingB || isFetchingB || isLoadingS || isFetchingS || isLoadingC || isFetchingC) 
-        return <div>Loading...</div>;
+  const specificBenefitId = "673f0042d993d72c4c06490c";
 
-    const specificBenefitId = "673f0042d993d72c4c06490c";
-    
-    const specificBenefit: Benefit | undefined = benefits?.find(benefit => benefit._id === specificBenefitId);
-    const specificSupplier:  Supplier | undefined = suppliers?.find(supplier => supplier._id === specificBenefit?.supplierId);
-    const specificClub: Club | undefined = clubs?.find(club => club._id === specificBenefit?.clubId);
+  const specificBenefit: Benefit | undefined = benefits?.find(
+    (benefit) => benefit._id === specificBenefitId
+  );
+  const specificSupplier: Supplier | undefined = suppliers?.find(
+    (supplier) => supplier._id === specificBenefit?.supplierId
+  );
+  const specificClub: Club | undefined = clubs?.find(
+    (club) => club._id === specificBenefit?.clubId
+  );
 
-
-    return (
-        <div>
-            <h1>Benefit Details</h1>
-            <div>
-                <strong>ID:</strong> {specificBenefit?._id}<br />
-                <strong>Description:</strong> {specificBenefit?.description}<br />
-                <strong>Supplier Logo:</strong> {specificSupplier?.supplierLogo}<br />
-                <strong>Club link:</strong> {specificClub?.clubLink}<br />
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Benefit Details</h1>
+      <div>
+        <strong>ID:</strong> {specificBenefit?._id}
+        <br />
+        <strong>Description:</strong> {specificBenefit?.description}
+        <br />
+        <strong>Supplier Logo:</strong> {specificSupplier?.supplierLogo}
+        <br />
+        <strong>Club link:</strong> {specificClub?.clubLink}
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default BenefitDetails;
