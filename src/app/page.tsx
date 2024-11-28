d3b978005e97b1519765751463afe260d1493179
Talia Friedman <taliafriedman22@gmail.com>
2024-11-28 09:26:14 +0200

    t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,6 @@ export default function Home() {
   return (
     <div>
       <p>Hello from page</p>
-      <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >     
-        </a>
     </div>
     
   );
