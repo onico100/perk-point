d875fe8165dfdd85b9dce7e6e6c8fc100e2d4737
Tamar-Amar <amtamar747@gmail.com>
2024-11-28 12:25:57 +0200

    css-top

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ import SideBar from "./Bars/SideBar";
 import TopBar from "./Bars/TopBar";
 import TopBarButtons from "./Bars/TopBarButtons";
 import TabSign from "./TabSign";
-import Login from "./Login";
+import Login from "./login";
 import ModePopup from "./Bars/ModePopup";
 import BenefitsCard from "./Benefits/BenefitCard";
 import BenefitsContainer from "./Benefits/BenefitsContainer";
