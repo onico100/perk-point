12ecb6ca5a8aa886c10a743134e62831ffe2d00f
Batya Cohen <batyah333@gmail.com>
2024-12-02 10:53:35 +0200

    middle

--- src/app/benefits/[clientId]/page.tsx
+++ src/app/benefits/[clientId]/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 import { BenefitsContainer } from "@/components";
 import useGeneralStore from "@/stores/generalStore";
-import { ClientMode, PreMode } from "@/types/types";
-
 
 export default function ClientId() {
   const setClientMode = useGeneralStore((state) => state.setClientMode);
   const setPreMode = useGeneralStore((state) => state.setPreMode);
-  const { clientMode } = useGeneralStore();
-
-  if (clientMode == ClientMode.connection) {
-    setClientMode(ClientMode.general);
-    setPreMode(PreMode.none);
-  }
 
   return <BenefitsContainer></BenefitsContainer>;
 }
--- src/components/Benefits/BenefitsContainer.tsx
+++ src/components/Benefits/BenefitsContainer.tsx
@@ -2,21 +2,50 @@
 import { useFetchBenefits } from "@/hooks/useFetchBenefits";
 import BenefitsCard from "@/components/Benefits/BenefitCard";
 import { useFetchSuppliers } from "@/hooks/useFetchSuppliers";
-import { Club, Supplier } from "@/types/types";
+import { Benefit, Club, Supplier } from "@/types/types";
 import { useFetchGeneral } from "@/hooks/useFetchGeneral";
 import styles from "@/styles/Benefits/BenefitsContainer.module.css";
+import useGeneralStore from "@/stores/generalStore";
+import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const BenefitsContainer = () => {
   const { benefits, isLoadingB, isFetchingB } = useFetchBenefits();
   const { suppliers } = useFetchSuppliers();
   const { clubs } = useFetchGeneral();
+  const { currentUser, clientMode } = useGeneralStore();
+
+  const [benefitsToShow, setBenefitsToShow] = useState<Benefit[]>([]);
+ 
+  const params = useParams();
+  const id = params.clientId;
+
+  useEffect(() => {
+    if (id !== "0") {
+      if (clientMode === "USER") {
+        setBenefitsToShow(
+          benefits?.filter((b: Benefit) => currentUser?.clubs.includes(b.clubId)) || []
+        );
+      } else if (clientMode === "SUPPLIER") {
+        setBenefitsToShow(
+          benefits?.filter((b: Benefit) => b.supplierId == id) || []
+        );
+      }
+    } else {
+      setBenefitsToShow(benefits || []);
+    }
+    console.log(benefits)
+    console.log(benefitsToShow)
+    console.log(id)
+  }, [benefits]);
 
   if (isLoadingB || isFetchingB) return <div>Loading...</div>;
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>כל הההטבות</div>
       <div className={styles.cardsContainer}>
-        {benefits?.map((benefit) => (
+        {benefitsToShow.map((benefit) => (
           <BenefitsCard
             key={benefit._id}
             benefit={benefit}
@@ -24,7 +53,7 @@ const BenefitsContainer = () => {
               (s: Supplier) => s._id === benefit?.supplierId
             )}
             club={clubs?.find((c: Club) => c._id == benefit.clubId)}
-          ></BenefitsCard>
+          />
         ))}
       </div>
     </div>
