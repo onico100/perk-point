a020e1a25b52b38364ef47e46d3e6edfa35b2d1a
onico100 <142163577+onico100@users.noreply.github.com>
2024-12-03 09:43:42 +0200

    calc button style

--- src/components/Calcutre/CalcButton.tsx
+++ src/components/Calcutre/CalcButton.tsx
@@ -1,38 +1,15 @@
 import React from "react";
 import { FaCalculator } from "react-icons/fa";
+import styles from "@/styles/Calc.module.css";
 
 const CalcButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
   return (
-    <div style={styles.container}>
-      <button style={styles.button} onClick={onClick}>
-        <FaCalculator style={styles.icon} />
+    <div className={styles.calcButton}>
+      <button onClick={onClick}>
+        <FaCalculator className={styles.calcButtonIcon} />
       </button>
     </div>
   );
-};
-
-const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    position: "fixed",
-    bottom: "20px",
-    left: "20px",
-  },
-  button: {
-    width: "60px",
-    height: "60px",
-    borderRadius: "50%",
-    border: "none",
-    background: "linear-gradient(to bottom right, #b346e8, #87cdfa)",
-    color: "white",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-    cursor: "pointer",
-  },
-  icon: {
-    fontSize: "24px",
-  },
 };
 
 export default CalcButton;
