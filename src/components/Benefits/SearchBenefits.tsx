205434e57cbf0170f2fa796dffdc49af96b45385
David Marom <davidmarom@hotmail.com>
2024-12-05 12:19:45 +0200

    Removed useEffect

--- src/components/Benefits/SearchBenefits.tsx
+++ src/components/Benefits/SearchBenefits.tsx
@@ -1,29 +1,10 @@
 //use effect need to be changed
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
-import {
-  SearchContainer,
-  SearchIcon,
-  InputContainer,
-  SupplierInput,
-  SelectContainer,
-  SelectLabel,
-  Dropdown,
-  DropdownOption,
-  DateLabel,
-  DateInput,
-} from "./SearchBenefits.Styles";
-import { RiSwapLine } from "react-icons/ri";
+import { SearchContainer, SearchIcon, InputContainer, SupplierInput, SelectContainer, SelectLabel, Dropdown, DropdownOption, DateLabel, DateInput } from "./SearchBenefits.Styles";
 
-interface Club {
-  _id: string;
-  clubName: string;
-}
-
-interface Category {
-  _id: string;
-  categoryName: string;
-}
+interface Club { _id: string; clubName: string; }
+interface Category { _id: string; categoryName: string; }
 
 interface SearchProps {
   clubs: Club[];
@@ -37,148 +18,72 @@ interface SearchProps {
   ) => void;
 }
 
-const SearchBenefits: React.FC<SearchProps> = React.memo(
-  ({ clubs, categories, onSearch }) => {
-    const [supplierFilter, setSupplierFilter] = useState("");
-    const [selectedClubs, setSelectedClubs] = useState<string[]>([]);
-    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-    const [branchFilter, setBranchFilter] = useState("");
-    const [expirationStart, setExpirationStart] = useState<Date | null>(null);
-    const [expirationEnd, setExpirationEnd] = useState<Date | null>(null);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [categoryDropdownOpen, setCategoryDropdownOpen] = useState(false);
+const SearchBenefits: React.FC<SearchProps> = (({ clubs, categories, onSearch }) => {
+  const [supplierFilter, setSupplierFilter] = useState("");
+  const [selectedClubs, setSelectedClubs] = useState<string[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [branchFilter, setBranchFilter] = useState("");
+  const [expirationStart, setExpirationStart] = useState<Date | null>(null);
+  const [expirationEnd, setExpirationEnd] = useState<Date | null>(null);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [categoryDropdownOpen, setCategoryDropdownOpen] = useState(false);
 
-    const memoizedOnSearch = useCallback(() => {
-      onSearch(
-        supplierFilter,
-        selectedClubs,
-        selectedCategories,
-        branchFilter,
-        [expirationStart, expirationEnd]
-      );
-    }, [
-      onSearch,
-      supplierFilter,
-      selectedClubs,
-      selectedCategories,
-      branchFilter,
-      expirationStart,
-      expirationEnd,
-    ]);
+  const doSearch = () => { onSearch(supplierFilter, selectedClubs, selectedCategories, branchFilter, [expirationStart, expirationEnd]) }
 
-    useEffect(() => {
-      memoizedOnSearch();
-    }, [
-      supplierFilter,
-      selectedClubs,
-      selectedCategories,
-      branchFilter,
-      expirationStart,
-      expirationEnd,
-      memoizedOnSearch,
-    ]);
+  const handleClubCheckboxChange = (clubId: string) => { setSelectedClubs((prev) => prev.includes(clubId) ? prev.filter((id) => id !== clubId) : [...prev, clubId]); doSearch() };
+  const handleCategoryCheckboxChange = (categoryId: string) => { setSelectedCategories((prev) => prev.includes(categoryId) ? prev.filter((id) => id !== categoryId) : [...prev, categoryId]); doSearch() };
+  const handleSupplierFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => { setSupplierFilter(e.target.value); doSearch() }
+  const handleBranchFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => { setBranchFilter(e.target.value); doSearch() }
+  const handleExpirationStartChange = (e: React.ChangeEvent<HTMLInputElement>) => { setExpirationStart(e.target.value ? new Date(e.target.value) : null); doSearch() }
+  const handleExpirationEndChange = (e: React.ChangeEvent<HTMLInputElement>) => { setExpirationEnd(e.target.value ? new Date(e.target.value) : null); doSearch() }
 
-    const handleClubCheckboxChange = (clubId: string) => {
-      setSelectedClubs((prev) =>
-        prev.includes(clubId)
-          ? prev.filter((id) => id !== clubId)
-          : [...prev, clubId]
-      );
-    };
+  return (
+    <SearchContainer>
 
-    const handleCategoryCheckboxChange = (categoryId: string) => {
-      setSelectedCategories((prev) =>
-        prev.includes(categoryId)
-          ? prev.filter((id) => id !== categoryId)
-          : [...prev, categoryId]
-      );
-    };
+      <InputContainer><SupplierInput onChange={handleSupplierFilterChange} type="text" placeholder="חיפוש שם העסק" value={supplierFilter} /><SearchIcon /></InputContainer>
 
-    return (
-      <SearchContainer>
-        <InputContainer>
-          <SupplierInput
-            type="text"
-            placeholder="חיפוש שם העסק"
-            value={supplierFilter}
-            onChange={(e) => setSupplierFilter(e.target.value)}
-          />
-          <SearchIcon />
-        </InputContainer>
-        <SelectContainer isOpen={dropdownOpen}>
-          <SelectLabel onClick={() => setDropdownOpen(!dropdownOpen)}>
-            {selectedClubs.length > 0
-              ? `${selectedClubs.length} מועדונים`
-              : "בחר מועדונים"}
-            <FaChevronDown />
-          </SelectLabel>
-          {dropdownOpen && (
-            <Dropdown>
-              {clubs.map((club) => (
-                <DropdownOption key={club._id}>
-                  <input
-                    type="checkbox"
-                    id={club._id}
-                    checked={selectedClubs.includes(club._id)}
-                    onChange={() => handleClubCheckboxChange(club._id)}
-                  />
-                  <label htmlFor={club._id}>{club.clubName}</label>
-                </DropdownOption>
-              ))}
-            </Dropdown>
-          )}
-        </SelectContainer>
-        <SelectContainer isOpen={categoryDropdownOpen}>
-          <SelectLabel
-            onClick={() => setCategoryDropdownOpen(!categoryDropdownOpen)}
-          >
-            {selectedCategories.length > 0
-              ? `${selectedCategories.length} קטגוריות`
-              : "בחר קטגוריות"}
-            <FaChevronDown />
-          </SelectLabel>
-          {categoryDropdownOpen && (
-            <Dropdown>
-              {categories?.map((category) => (
-                <DropdownOption key={category._id}>
-                  <input
-                    type="checkbox"
-                    id={category._id}
-                    checked={selectedCategories.includes(category._id)}
-                    onChange={() => handleCategoryCheckboxChange(category._id)}
-                  />
-                  <label htmlFor={category._id}>{category.categoryName}</label>
-                </DropdownOption>
-              ))}
-            </Dropdown>
-          )}
-        </SelectContainer>
-        <InputContainer>
-          <SupplierInput
-            type="text"
-            placeholder="חיפוש לפי סניף"
-            value={branchFilter}
-            onChange={(e) => setBranchFilter(e.target.value)}
-          />
-          <SearchIcon />
-        </InputContainer>
-        <DateLabel>תוקף מ:</DateLabel>
-        <DateInput
-          type="date"
-          onChange={(e) =>
-            setExpirationStart(e.target.value ? new Date(e.target.value) : null)
-          }
-        />
-        <DateLabel>עד:</DateLabel>
-        <DateInput
-          type="date"
-          onChange={(e) =>
-            setExpirationEnd(e.target.value ? new Date(e.target.value) : null)
-          }
-        />
-      </SearchContainer>
-    );
-  }
+      <SelectContainer isOpen={dropdownOpen}>
+        <SelectLabel onClick={() => setDropdownOpen(!dropdownOpen)}>{selectedClubs.length > 0 ? `${selectedClubs.length} מועדונים` : "בחר מועדונים"}<FaChevronDown />
+        </SelectLabel>
+
+        {dropdownOpen && (
+          <Dropdown>
+            {clubs.map((club) => (
+              <DropdownOption key={club._id}>
+                <input onChange={() => handleClubCheckboxChange(club._id)} type="checkbox" id={club._id} checked={selectedClubs.includes(club._id)} />
+                <label htmlFor={club._id}>{club.clubName}</label>
+              </DropdownOption>
+            ))}
+          </Dropdown>
+        )}
+      </SelectContainer>
+
+      <SelectContainer isOpen={categoryDropdownOpen}>
+        <SelectLabel onClick={() => setCategoryDropdownOpen(!categoryDropdownOpen)}>
+          {selectedCategories.length > 0 ? `${selectedCategories.length} קטגוריות` : "בחר קטגוריות"}
+          <FaChevronDown />
+        </SelectLabel>
+        
+        {categoryDropdownOpen && (
+          <Dropdown>
+            {categories?.map((category) => (
+              <DropdownOption key={category._id}>
+                <input onChange={() => handleCategoryCheckboxChange(category._id)} type="checkbox" id={category._id} checked={selectedCategories.includes(category._id)} />
+                <label htmlFor={category._id}>{category.categoryName}</label>
+              </DropdownOption>
+            ))}
+          </Dropdown>
+        )}
+      </SelectContainer>
+
+      <InputContainer><SupplierInput onChange={handleBranchFilterChange} type="text" placeholder="חיפוש לפי סניף" value={branchFilter} /><SearchIcon /></InputContainer>
+
+      <DateLabel>תוקף מ:</DateLabel><DateInput type="date" onChange={handleExpirationStartChange} />
+      <DateLabel>עד:</DateLabel><DateInput type="date" onChange={handleExpirationEndChange}
+      />
+    </SearchContainer>
+  );
+}
 );
 
 export default SearchBenefits;
