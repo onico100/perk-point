a0d28b41b58b738b30aeff82dc6ad087a4915c2a
onico100 <142163577+onico100@users.noreply.github.com>
2024-12-09 13:48:29 +0200

    footer style

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -1,39 +1,63 @@
 // components/Footer.tsx
 import React from "react";
 import styles from "@/styles/Home/Footer.module.css";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div>
+        {/* Brand/Info Section */}
+        <div className={styles.brand}>
           <p>&copy; {new Date().getFullYear()} Perk Point</p>
           <p>כל הזכויות שמורות.</p>
         </div>
-        <nav>
-          <h2>מפת האתר</h2>
-          <ul className={styles.navLinks}>
-            <li>
-              <a href="/">בית</a>
-            </li>
-            <li>
-              <a href="/about">אודות</a>
-            </li>
-            <li>
-              <a href="/contact">צור קשר</a>
-            </li>
-          </ul>
-        </nav>
-        <nav>
-          <ul className={styles.navLinks}>
-            <li>
-              <a href="/privacy">מדינות פרטיות</a>
-            </li>
-            <li>
-              <a href="/terms">תקנון האתר</a>
-            </li>
-          </ul>
-        </nav>
+
+        {/* Navigation Links */}
+        <div className={styles.links}>
+          <div className={styles.linkColumn}>
+            <h3>מפת האתר</h3>
+            <ul>
+              <li>
+                <a href="/">בית</a>
+              </li>
+              <li>
+                <a href="/about">אודות</a>
+              </li>
+              <li>
+                <a href="/contact">צור קשר</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.linkColumn}>
+            <h3>מידע</h3>
+            <ul>
+              <li>
+                <a href="/privacy">מדיניות פרטיות</a>
+              </li>
+              <li>
+                <a href="/terms">תקנון האתר</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Social Media Section */}
+        <div className={styles.socialMedia}>
+          <h3>עקבו אחרינו</h3>
+          <div className={styles.socialIcons}>
+            <a href="#" aria-label="Facebook">
+              <FaFacebook className={styles.facebook} />
+            </a>
+            <a href="#" aria-label="Twitter">
+              <FaTwitter className={styles.twitter} />
+            </a>
+            <a href="#" aria-label="Instagram">
+              <FaSquareInstagram className={styles.instegram} />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
