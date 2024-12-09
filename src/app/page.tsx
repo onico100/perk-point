6f6f5157671cedf06a66a7acb72ba38f17ea16a8
onico100 <142163577+onico100@users.noreply.github.com>
2024-12-09 09:36:00 +0200

    adding title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 import useGeneralStore from "@/stores/generalStore";
 import { ClientMode, PreMode } from "@/types/types";
 import { HomeComp } from "@/components/index";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "PerkPoint";
+  }, []);
+
   const setClientMode = useGeneralStore((state) => state.setClientMode);
 
   const { clientMode } = useGeneralStore();
