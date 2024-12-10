1f59d93a787bc39a83bdb1d3bbb295b7aa3c9778
onico100 <142163577+onico100@users.noreply.github.com>
2024-12-10 15:05:41 +0200

    loose use uffect

--- src/app/benefits/[clientId]/page.tsx
+++ src/app/benefits/[clientId]/page.tsx
@@ -5,48 +5,36 @@ import { useFetchBenefits } from "@/hooks/useFetchBenefits";
 import useGeneralStore from "@/stores/generalStore";
 import { Benefit, ClientMode } from "@/types/types";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function ClientId() {
   const setClientMode = useGeneralStore((state) => state.setClientMode);
   const { benefits, isLoadingB, isFetchingB } = useFetchBenefits();
   const { currentUser, clientMode } = useGeneralStore();
-  const [benefitsToShow, setBenefitsToShow] = useState<Benefit[]>([]);
   const titles = ["כל ההטבות", "ההטבות שלי", "הטבות החברה"];
-  const [currentTitle, setCurrentTitle] = useState(titles[0]);
   const params = useParams();
   const id = params.clientId;
 
-  useEffect(() => {
-    console.log(33, "benefits:", benefits);
-    console.log(33, "currentUser:", currentUser);
-    console.log(33, "clientMode:", clientMode);
-
-    if (!benefits) return;
-
-    if (id !== "0") {
-      if (clientMode === "USER") {
-        const userBenefits =
-          benefits.filter((b) => currentUser?.clubs.includes(b.clubId)) || [];
-        setBenefitsToShow(userBenefits);
-        setCurrentTitle(titles[1]);
-      } else if (clientMode === "SUPPLIER") {
-        console.log("supplierId:", id);
-        const supplierBenefits =
-          benefits.filter((b) => b.supplierId === id) || [];
-        setBenefitsToShow(supplierBenefits);
-        setCurrentTitle(titles[2]);
-      }
-    } else {
-      setBenefitsToShow(benefits); // Default: show all benefits
-      setCurrentTitle(titles[0]);
-      console.log(33, "benefitsAfter", benefits);
-      console.log(33, "benefitToShow", benefitsToShow);
-    }
-  }, [benefits, currentUser, clientMode, id]);
-
   // Ensure general mode is set if in connection mode
   if (clientMode === ClientMode.connection) setClientMode(ClientMode.general);
+
+  // Calculate benefits to show and the title
+  let benefitsToShow: Benefit[] = [];
+  let currentTitle = titles[0];
+
+  if (benefits) {
+    if (id !== "0") {
+      if (clientMode === "USER") {
+        benefitsToShow =
+          benefits.filter((b) => currentUser?.clubs.includes(b.clubId)) || [];
+        currentTitle = titles[1];
+      } else if (clientMode === "SUPPLIER") {
+        benefitsToShow = benefits.filter((b) => b.supplierId === id) || [];
+        currentTitle = titles[2];
+      }
+    } else {
+      benefitsToShow = benefits; // Default: show all benefits
+    }
+  }
 
   if (isLoadingB || isFetchingB) return <LoadingSpinner />;
 
