0265a6967e0a99f3cabdda72710547e3e1246b44
Tamar-Amar <amtamar747@gmail.com>
2024-12-11 09:28:58 +0200

    preMode-delete

--- src/app/login-supplier/page.tsx
+++ src/app/login-supplier/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import LoginSupplierComponent from "@/components/SignPages/loginSupplier";
-import useGeneralStore from "@/stores/generalStore";
 import styles from "@/styles/SignPages/sign.module.css";
-import { ClientMode, PreMode } from "@/types/types";
 export default function LoginSupplierPage() {
 
   return (
