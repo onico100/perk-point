17e07440b3cbf04c2a767325c7edb21a879f64b7
onico100 <142163577+onico100@users.noreply.github.com>
2024-12-17 13:17:35 +0200

    http

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,15 +1,11 @@
 import axios from "axios";
 
 const my_http = axios.create({
+  url: "/api",
   baseURL: "/api",
   headers: {
-    "Content-Type": "application/json",
-    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-    Pragma: "no-cache",
-    Expires: "0",
-  },
-  params: {
-    _t: new Date().getTime(),
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 });
 
